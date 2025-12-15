37b78f948805fb7b81df7dc094274b8bcae34e83
Davi Hermidas Lage Brandão <hermidasdavi@gmail.com>
2025-12-14 21:44:34 -0300

    tudo

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+// Porta dinâmica do Render
+const PORT = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// Middleware CORS (IMPORTANTE)
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  next();
+});
+
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`🚀 JSON Server rodando na porta ${PORT}`);
+});
 
